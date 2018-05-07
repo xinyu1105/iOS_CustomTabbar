1ac30af27befdd719d01ef2d4c623256f7aa0c95
jiaxin.peng <jiaixn_1105@163.com>
2018-05-07 11:29:33 +0800

    init ios custom Tabbar

---
+++ iOS_CustomTabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_CustomTabbar
+//
+//  Created by pengjiaxin on 2018/5/7.
+//  Copyright © 2018年 pengjiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_CustomTabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_CustomTabbar
+//
+//  Created by pengjiaxin on 2018/5/7.
+//  Copyright © 2018年 pengjiaxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
